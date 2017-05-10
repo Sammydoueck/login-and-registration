4d47ab15f4104350d1e5a62a98679410235fddc5
Sammy Doueck <sammydoueck@Sammys-Air-2.home>
2017-05-10 18:31:55 -0400

    first commit

---
+++ Login and Registration/RegisterPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterPageViewController.swift
+//  Login and Registration
+//
+//  Created by Sammy Doueck on 5/9/17.
+//  Copyright © 2017 Sammy Doueck. All rights reserved.
+//
+
+import UIKit
+
+class RegisterPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
